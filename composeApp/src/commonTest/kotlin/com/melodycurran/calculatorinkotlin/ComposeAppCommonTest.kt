e0200d2b359f94e59ad59049fcd4433d4ad1b504
melodycurran <97179039+melodycurran@users.noreply.github.com>
2026-05-21 07:49:53 -0600

    new KMP boilerplate

---
+++ composeApp/src/commonTest/kotlin/com/melodycurran/calculatorinkotlin/ComposeAppCommonTest.kt
@@ -0,0 +1,12 @@
+package com.melodycurran.calculatorinkotlin
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ComposeAppCommonTest {
+
+    @Test
+    fun example() {
+        assertEquals(3, 1 + 2)
+    }
+}
---
+++ composeApp/src/iosMain/kotlin/com/melodycurran/calculatorinkotlin/MainViewController.kt
@@ -0,0 +1,5 @@
+package com.melodycurran.calculatorinkotlin
+
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun MainViewController() = ComposeUIViewController { App() }
